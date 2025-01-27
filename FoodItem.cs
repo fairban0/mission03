08c86090177468a8fe061e79f12df22ed74a4848
fairban0 <fairban0@byu.edu>
2025-01-27 09:30:24 -0700

    Creating Class

---
+++ FoodItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mission03
+{
+    public class FoodItem
+    {
+        // Properties of a food item
+        public string foodName { get; set; }
+        public string foodCategory { get; set; }
+        public int foodQuantity { get; set; }
+        public DateTime foodExpireDate { get; set; }
+
+        // Constructor for creating a FoodItem object
+        public FoodItem(string name, string category, int quantity, DateTime expireDate)
+        {
+            foodName = name;
+            foodCategory = category;
+            foodQuantity = quantity;
+            foodExpireDate = expireDate;
+        }
+
+        public override string ToString()
+        {
+            return $"{foodName} - {foodCategory} - {foodQuantity} - {foodExpireDate.ToShortDateString()}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
  */
 using System;
 
+namespace mission03 { 
 class Program
 {
     static void Main(string[] args)
@@ -190,26 +191,5 @@ class Program
         }
     }
 }
-
-class FoodItem
-{
-    // Properties of a food item
-    public string foodName { get; set; }
-    public string foodCategory { get; set; }
-    public int foodQuantity { get; set; }
-    public DateTime foodExpireDate { get; set; }
-
-    // Constructor for creating a FoodItem object
-    public FoodItem(string name, string category, int quantity, DateTime expireDate)
-    {
-        foodName = name;
-        foodCategory = category;
-        foodQuantity = quantity;
-        foodExpireDate = expireDate;
-    }
-
-    public override string ToString()
-    {
-        return $"{foodName} - {foodCategory} - {foodQuantity} - {foodExpireDate.ToShortDateString()}";
-    }
 }
+
